40d9224c5d2b767e7a1c848fea0e950d38aa25fb
PEACE-DFG <awofesobipeace@gmail.com>
2023-05-10 02:22:54 +0100

    done first ass

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App(){
+  return(
+    <div>
+      <h1>About Myself</h1>
+      <div>
+        <img src="https://previews.123rf.com/images/prettyvectors/prettyvectors1702/prettyvectors170200022/71079316-young-programmer-man-character-coding-vector-flat-cartoon-illustration.jpg" alt="" />
+        <p>
+          My Name is CODEMaster. My Passion is all About codes. The experirence with my system feels so unexplanable it is always like i have found a better way of expressing myself. I am currently a student of Osun State University, Department Of Electrical Electronic Engineering. 
+        </p>
+      </div>
+    </div>
+  )
+
+}
+
+export default App
